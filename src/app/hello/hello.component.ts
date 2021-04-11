17057c00208db14780ceb7dad8be20ce132b6815
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2021-04-11 20:52:57 -0300

    Initialize Project

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'hello',
+    template: `
+        <h1>Hello World!</h1>
+        <h2>Lorem Ipsum</h2>
+    `
+})
+
+export class HelloComponent {
+
+}
